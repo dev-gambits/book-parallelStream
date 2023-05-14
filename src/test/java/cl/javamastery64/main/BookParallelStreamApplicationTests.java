414f258ac91bb5e3386b630765c46deeea0f8e7d
dev-gambits <emmanuel.nieto.munoz@ciisa.cl>
2023-05-14 04:06:22 -0400

    Inicio proyecto

---
+++ src/test/java/cl/javamastery64/main/BookParallelStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.javamastery64.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookParallelStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
